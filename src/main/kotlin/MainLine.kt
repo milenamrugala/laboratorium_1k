5d4cf7f6c20b66a0e2c70cfbfa105c4557391506
milenamrugala <milena.mrugala@icloud.com>
2024-03-03 17:35:52 +0100

    MainLine

--- src/main/kotlin/MainLine.kt
+++ src/main/kotlin/MainLine.kt
@@ -1,1 +1,29 @@
 // Milena Mrugała, zadanie nr 2
+
+import javax.swing.JFrame
+fun main()  {
+
+    val lines = arrayOf(
+        Line(0, 0, 600, 0), //AB
+        Line(600, 0, 600, 300), //BC
+        Line(600, 300, 300, 600), //CE
+        Line(300, 600, 0, 300), //ED
+        Line(0, 300, 0, 0), //DA
+        Line(0, 0, 600, 300), //AC
+        Line(600, 300, 0, 300), //CD
+        Line(0, 300, 600, 0) //DB
+    )
+
+    for ((index, line) in lines.withIndex()) {
+        println("Linia ${index + 1}: $line")
+    }
+
+    val frame = JFrame("Envelope Visualization")
+    frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+    frame.setSize(700, 700)
+
+    val drawingPanel = Envelope(lines.toList())
+    frame.contentPane.add(drawingPanel)
+
+    frame.isVisible = true
+}
