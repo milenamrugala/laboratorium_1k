fc2b027bde4fb71e2f93d9f52082ad2b030f13c5
milenamrugala <milena.mrugala@icloud.com>
2024-03-03 13:52:33 +0100

    Student class

---
+++ src/main/kotlin/Student.kt
@@ -0,0 +1,33 @@
+// Milena Mrugała, zadanie nr 3
+
+class Student(
+    private var firstName: String,
+    private var lastName: String,
+    private var id: String
+) {
+
+    override fun toString(): String {
+        return "Student(firstName='$firstName', lastName='$lastName', id='$id')"
+    }
+
+    fun getFirstName(): String {
+        return firstName
+    }
+
+    fun getLastName(): String {
+        return lastName
+    }
+
+    fun getId(): String {
+        return id
+    }
+
+    fun setFirstName(firstName: String) {
+        this.firstName = firstName
+    }
+
+    fun setLastName(lastName: String) {
+        this.lastName = lastName
+
+    }
+}
